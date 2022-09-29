552b523bc569db0229d9268726914c08c067ec1e
wangzhihong <854601776@qq.com>
2022-09-29 10:25:03 +0800

    保存

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,14 +1,14 @@
-$(function(){
+$(function () {
 
 
-  $(`.go2reg`).on('click',function(){
+  $(`.go2reg`).on('click', function () {
     $(`.reg-wrap`).hide()
     $(`.login-wrap`).show()
     console.log(1);
 
   })
 
-  $(`.go2login`).on('click',function(){
+  $(`.go2login`).on('click', function () {
     console.log(1);
     $(`.reg-wrap`).show()
     $(`.login-wrap`).hide()
@@ -16,7 +16,17 @@ $(function(){
   })
 
 
+  const form = layui.form
+  form.verify({
+    pass: [
+      /^[\S]{6,12}$/
+      , '密码必须6到12位，且不能出现空格'
+    ],
+    same: function (value) {
+      if ($(`#password`).val() != value) return `2次密码不一样`
+    }
 
+  })
 
 
 
